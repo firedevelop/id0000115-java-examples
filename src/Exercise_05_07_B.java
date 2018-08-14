feac78fc1f36737e42e23d2272bc16efb81c3f1e
firedevelop <firedevelop.com@gmail.com>
2018-08-14 15:13:00 +0200

    Commit message

--- src/Exercise_05_07_B.java
+++ src/Exercise_05_07_B.java
@@ -1,5 +1,7 @@
 public class Exercise_05_07_B {
-    public static void main(String[args]){
+    public static void main(String[]args){
+        System.out.println("hola");
+        System.out.println("hi");
 
     }
 }
